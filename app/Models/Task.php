1785da01ceb38d3d386f516375986dd2ce5aae39
Mu'adz Mohd Rosli <muadz.mrosli@gmail.com>
2023-09-26 09:55:00 +0800

    feat: create task migration and model

---
+++ app/Models/Task.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\TaskStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'description', 'status'];
+
+    protected $casts = [
+        'status' => TaskStatus::class
+    ];
+}
